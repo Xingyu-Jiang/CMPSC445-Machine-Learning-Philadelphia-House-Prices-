581ed4d2e562d2987b049e6e156cc35cf608cdb0
Xingyu-Jiang <xzj5086@psu.edu>
2024-03-06 22:21:12 -0500

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.pipeline import Pipeline
